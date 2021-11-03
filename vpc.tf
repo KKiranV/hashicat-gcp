e4e88c22ad649ad31129f3461f88f8c58537a5fa
KiranM <vaswanikiran@yahoo.co.in>
2021-11-03 18:39:23 +0000

    adding vpc module for gaurav 4

--- vpc.tf
+++ vpc.tf
@@ -1,44 +1,15 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
+module "network" {
+  source  = "app.terraform.io/KiranMack/network/google"
+  version = "3.4.0"
+  # insert required variables here
+  network_name = "gaurav-vpc"
+  project_id = var.project
 
-    project_id   = var.project
-    network_name = "terraform-gaurav-vpc"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-        {
-            subnet_name           = "gaurav-subnet-01"
-            subnet_ip             = "10.100.10.0/24"
-            subnet_region         = var.region
-        },
-     ]
-
-    secondary_ranges = {
-        gaurav-subnet-01 = [
-            {
-                range_name    = "subnet-01-secondary-01"
-                ip_cidr_range = "192.168.64.0/24"
-            },
-        ]
-
-    }
-
-    routes = [
-        {
-            name                   = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-            next_hop_internet      = "true"
-        },
-        {
-            name                   = "app-proxy"
-            description            = "route through proxy to reach app"
-            destination_range      = "10.50.10.0/24"
-            tags                   = "app-proxy"
-            next_hop_instance      = "app-proxy-instance"
-            next_hop_instance_zone = "us-west1-a"
-        },
-    ]
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+ ]
 }
